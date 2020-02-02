511c9b844cb54b55084eac36350ab9c44016c718
GambuzX <mg_mesquita@hotmail.com>
2020-02-02 12:46:39 +0000

    death sequence

--- project/Assets/Scripts/CollisionHandler.cs
+++ project/Assets/Scripts/CollisionHandler.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+
+    [Tooltip("FX prefab on player")][SerializeField] GameObject explosion;
     
     private void OnTriggerEnter(Collider other) {
         StartDeathSequence();
     }
 
     private void StartDeathSequence() {
-        print("RIP");
         SendMessage("OnPlayerDeath");
+        explosion.SetActive(true);     
+        Invoke("ReloadScene", levelLoadDelay);   
+    }
+
+    private void ReloadScene() {
+        SceneManager.LoadScene(1);
     }
 }
